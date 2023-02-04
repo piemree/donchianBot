032fa71c7ea275a2666233ddedc1437243f2b7fd
piemree <pi.emree@gmail.com>
2023-02-04 12:28:45 +0300

    add commands

---
+++ commands.js
@@ -0,0 +1,102 @@
+const TelegramBot = require("node-telegram-bot-api");
+const { telegram, botConfig } = require("./config");
+const b = require("./helpers/binance");
+const exec = require("child_process").exec;
+
+const bot = new TelegramBot(telegram.token, { polling: true });
+
+bot.onText(/stop/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Trade Bot Stopping...");
+  exec("pm2 stop bot", (error, stdout, stderr) => {
+    if (error) {
+      bot.sendMessage(msg.chat.id, "Failed to stop Trade Bot.");
+    } else {
+      bot.sendMessage(msg.chat.id, "Trade Bot Stopped Successfully.");
+    }
+  });
+});
+
+bot.onText(/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Trade Bot Starting...");
+  exec("pm2 start bot", (error, stdout, stderr) => {
+    if (error) {
+      bot.sendMessage(msg.chat.id, "Failed to start Trade Bot.");
+    } else {
+      bot.sendMessage(msg.chat.id, "Trade Bot Started Successfully.");
+    }
+  });
+});
+
+bot.onText(/kill/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Trade Bot Deleting...");
+  exec("pm2 delete bot", (error, stdout, stderr) => {
+    if (error) {
+      bot.sendMessage(msg.chat.id, "Failed to delete Trade Bot.");
+    } else {
+      bot.sendMessage(msg.chat.id, "Trade Bot Deleted Successfully.");
+    }
+  });
+});
+
+bot.onText(/init/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Trade Bot Initializing...");
+  exec("pm2 start app.js --name bot", (error, stdout, stderr) => {
+    if (error) {
+      bot.sendMessage(msg.chat.id, "Failed to initialize Trade Bot.");
+    } else {
+      bot.sendMessage(msg.chat.id, "Trade Bot Initialized Successfully.");
+    }
+  });
+});
+
+bot.onText(/restart/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Trade Bot Restarting...");
+  exec("pm2 restart bot", (error, stdout, stderr) => {
+    if (error) {
+      bot.sendMessage(msg.chat.id, "Failed to restart Trade Bot.");
+    } else {
+      bot.sendMessage(msg.chat.id, "Trade Bot Restarted Successfully.");
+    }
+  });
+});
+
+bot.onText(/balance/, (msg) => {
+  b.getBalance().then((balance) => {
+    bot.sendMessage(msg.chat.id, balance);
+  });
+});
+
+bot.onText(/positions/, (msg) => {
+  b.findPositions({
+    symbol: botConfig.symbol,
+  })
+    .then((status) => {
+      if (!status.LongPosition && !status.ShortPosition) {
+        bot.sendMessage(msg.chat.id, "No open positions");
+        return;
+      }
+      bot.sendMessage(msg.chat.id, JSON.stringify(status, null, 2));
+    })
+    .catch((error) => {
+      bot.sendMessage(msg.chat.id, JSON.stringify(error.body, null, 2));
+    });
+});
+
+// send available commands when /help is received
+bot.onText(/help/, (msg) => {
+  // send as html available commands
+  bot.sendMessage(
+    msg.chat.id,
+    `
+    <b>Available commands:</b>
+    <code>/balance</code> - get current balance
+    <code>/positions</code> - get current positions
+    <code>/start</code> - start trade bot
+    <code>/stop</code> - stop trade bot
+    <code>/kill</code> - kill trade bot
+    <code>/init</code> - initialize trade bot
+    <code>/restart</code> - restart trade bot
+    `,
+    { parse_mode: "HTML" }
+  );
+});
