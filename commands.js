09d366dc4d5ddb24e6c79de874a047cafec593b8
piemree <pi.emree@gmail.com>
2023-02-04 12:55:01 +0300

    add new commands

--- commands.js
+++ commands.js
@@ -90,14 +90,13 @@ bot.onText(/update/, (msg) => {
       bot.sendMessage(msg.chat.id, "Failed to fetch.");
     } else {
       bot.sendMessage(msg.chat.id, "Fetch Successfully.");
-    }
-  });
-
-  exec("pm2 restart bot && pm2 save", (error, stdout, stderr) => {
-    if (error) {
-      bot.sendMessage(msg.chat.id, "Failed to restart Trade Bot.");
-    } else {
-      bot.sendMessage(msg.chat.id, "Trade Bot Restarted Successfully.");
+      exec("pm2 restart bot && pm2 save", (error, stdout, stderr) => {
+        if (error) {
+          bot.sendMessage(msg.chat.id, "Failed to restart Trade Bot.");
+        } else {
+          bot.sendMessage(msg.chat.id, "Trade Bot Restarted Successfully.");
+        }
+      });
     }
   });
 });
