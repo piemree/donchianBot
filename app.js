ecdec2c604155890374cdc7c262ef60f013c478d
piemree <pi.emree@gmail.com>
2023-03-09 16:36:26 +0300

    disable socket

--- app.js
+++ app.js
@@ -2,26 +2,21 @@ const main = require("./main");
 const config = require("./config");
 const { sendMessage } = require("./helpers/telegram");
 
-const { WebsocketClient } = require("binance");
+// create while loop that run every 5 seconds and run the main function
 
-const wsClient = new WebsocketClient({
-  api_key: config.API_KEY,
-  api_secret: config.API_SECRET,
-  beautify: true,
-});
-
-wsClient.subscribeKlines(
-  config.botConfig.symbol,
-  config.botConfig.interval,
-  "usdm"
-);
-
-wsClient.on("message", (msg) => {
-  if (msg?.k?.x || true) {
+(async () => {
+  while (true) {
+    console.log("Running main function");
     main(config.botConfig).catch((error) => {
       const today = new Date().toLocaleString();
       console.log(today, error);
       sendMessage(JSON.stringify(error, null, 2));
     });
+    await sleep(5000);
   }
-});
+
+  // sleep function
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+})();
