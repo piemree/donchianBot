8a5effb7a98891edb472249b546c930455afe8f9
piemree <pi.emree@gmail.com>
2023-03-11 01:44:08 +0300

    fix bug

--- main.js
+++ main.js
@@ -30,27 +30,18 @@ async function main({
   let noPosition = !LongPosition && !ShortPosition;
 
   const close = ohlc[ohlc.length - 1].close;
-
+  const atrs = ta.atr({ input: ohlc, period: atrPeriod });
+  const rsis = ta.rsi({ input: ohlc, period: rsiPeriod });
+  const rsi = rsis[rsis.length - 1];
+  const atr = atrs[atrs.length - 1];
   if (noPosition) {
-    const atrs = ta.atr({ input: ohlc, period: atrPeriod });
-    const rsis = ta.rsi({ input: ohlc, period: rsiPeriod });
     const entryMas = ta.sma({ input: ohlc, period: entryMaPeriod });
-    const exitMas = ta.sma({ input: ohlc, period: exitMaPeriod });
-    const rsi = rsis[rsis.length - 1];
-    const atr = atrs[atrs.length - 1];
     const entryMa = entryMas[entryMas.length - 1];
-    const exitMa = exitMas[exitMas.length - 1];
     const entryAtrValue = atr * entryAtrMultiplier;
-    const exitAtrValue = atr * exitAtrMultiplier;
     const entryLongMatr = entryMa + entryAtrValue;
     const entryShortMatr = entryMa - entryAtrValue;
-    const exitLongMatr = exitMa - exitAtrValue;
-    const exitShortMatr = exitMa + exitAtrValue;
-
     const longCondition = rsi > rsiUpper && close > entryLongMatr;
     const shortCondition = rsi < rsiLower && close < entryShortMatr;
-    const exitLongCondition = close < exitLongMatr;
-    const exitShortCondition = close > exitShortMatr;
 
     const quantity = parseFloat((balance * entryPercentage) / close);
     if (longCondition) {
@@ -60,6 +51,14 @@ async function main({
       await b.shortPosition({ symbol, quantity });
     }
   } else {
+    const exitMas = ta.sma({ input: ohlc, period: exitMaPeriod });
+    const exitMa = exitMas[exitMas.length - 1];
+    const exitAtrValue = atr * exitAtrMultiplier;
+    const exitLongMatr = exitMa - exitAtrValue;
+    const exitShortMatr = exitMa + exitAtrValue;
+    const exitLongCondition = close < exitLongMatr;
+    const exitShortCondition = close > exitShortMatr;
+
     if (LongPosition) {
       const quantity = Math.abs(parseFloat(LongPosition?.positionAmt));
       if (exitLongCondition) {
