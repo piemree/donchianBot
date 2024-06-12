26f41a92c99b80861449265bccc3dd3da5cdfacb
emre <pi.emree@gmail.com>
2024-06-12 21:42:54 +0300

    update settings

--- config/index.js
+++ config/index.js
@@ -1,15 +1,10 @@
 module.exports = {
-  // API_KEY: "0e13270863fe20ee10f4dc669c7fe8e931035858388b6900575f162ae941358a",
-  // API_SECRET:
-  //   "73fac1b8a5ad01851f43a01ff7bba25646363994d8cc777ff9bfeca74dd65112",
-  // baseUrl: "https://testnet.binancefuture.com",
-  API_KEY: "mfiaqGzJYgBmmh6zqKagMEIR79fR07I6xI8FAqMuXvJ7kQXEA0uOo3iyU1hzJbFK",
-  API_SECRET:
-    "6ifyw6Uy85H5gG8USceg5J28QGSgIFA9Zw6S5aPNPdJ2ed4L9hdBpuZVQKq1VbK3",
+  API_KEY: process.env.API_KEY,
+  API_SECRET: process.env.API_SECRET,
   baseUrl: "https://fapi.binance.com",
   telegram: {
-    chatId: 1431222577,
-    token: "6052310709:AAHQbXnPewPFKhwFlr9ZNR-yanF_NDC3Mbk",
+    chatId: process.env.TELEGRAM_CHAT_ID,
+    token: process.env.TELEGRAM_TOKEN,
   },
   botConfig: {
     asset: "USDT",
